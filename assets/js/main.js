120adfcd0744495480ca4634ad060039d7bd0605
tyleryandt18 <tyyandt@davidson.edu>
2023-09-16 14:10:45 -0400

    added first text

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,10 @@
-
 /* javascript */
-console.log("Although this series of ads was never officially launched, ",
+console.log("Although this series of ads was never officially launched,",
 "I wanted to focus my Unbranding Assignment on the JUUL marketing campaign",
-" because even the idea that this was considered is frightening. In fact, ",
-"according to the New York Times, this ad campaign was dropped for a one in ",
+"because even the idea that this was considered is frightening. In fact,",
+"according to the New York Times, this ad campaign was dropped for a one in",
 "which attractive models were posing with JUULs as a way to target a younger",
 "audience. I wanted to bring light to the fact that any vaping company shouldn't",
-" be advertising as 'the new cool thing'; they shouldn't be advertising at all.", 
-" When I was abroad, stores in Europe would show the potential risks of smoking ",
+"be advertising as 'the new cool thing'; they shouldn't be advertising at all.", 
+"When I was abroad, stores in Europe would show the potential risks of smoking",
 "on the actual package.")
